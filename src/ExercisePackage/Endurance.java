a74bfee6b510b0bdc36a8941045b9c3925061007
Even Matre Larsen <even@Evens-MacBook-Pro.local>
2020-04-01 14:31:37 +0200

    Fixed ending bracket

--- src/ExercisePackage/Endurance.java
+++ src/ExercisePackage/Endurance.java
@@ -4,7 +4,7 @@ public class Endurance extends Exercise {
 
     String equipment;
 
-    public Endurance(int intensity, int duration, int repetitions, int sets){
+    public Endurance(int intensity, int duration, int repetitions, int sets) {
         super(intensity, duration, repetitions, sets);
 
         this.equipment = equipment;
@@ -14,4 +14,5 @@ public class Endurance extends Exercise {
     public String toString() {
         return "Intensity:" + intensity + "\n" + "Duration: " + duration + "\n" + "Repetitions: " +
                 repetitions + ", Sets" + sets + "\n" + "Equipment: " + equipment;
+    }
 }
