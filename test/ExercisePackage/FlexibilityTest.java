e3340c156b1c376e998bef349c25fc9e1afdd09d
pkjeserud <pederkjeserud@gmail.com>
2020-04-02 11:00:25 +0200

    Changes to flexibility test

--- test/ExercisePackage/FlexibilityTest.java
+++ test/ExercisePackage/FlexibilityTest.java
@@ -1,7 +1,17 @@
 package ExercisePackage;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class FlexibilityTest {
+
+    @Test
+    public void testToString() {
+
+        Flexibility testFlexibility = new Flexibility(5, 30, 3, 5, "Yoga mat");
+
+        assertEquals();
+    }
 
 }
