70630a6133c9157f8ed2c4bc19286cc5d4571877
Even Matre Larsen <even@Evens-MacBook-Pro.local>
2020-04-02 11:50:10 +0200

    added test to intesity value to endurance

--- test/ExercisePackage/EnduranceTest.java
+++ test/ExercisePackage/EnduranceTest.java
@@ -7,7 +7,21 @@ import static org.junit.Assert.*;
 public class EnduranceTest {
 
     @Test
-    public void toString() {
+    public void TestToString() {
 
+        Endurance testEndurance = new Endurance("Lil E", 5, 30, 3, 5, "Running Shoes");
+
+        assertEquals("Test to check that string equipment works", "Running Shoes", testEndurance.equipment);
+    }
+    @Test
+    public void numberTest(){
+
+        Endurance testEndurance = new Endurance("Lil E", 11, 5, 5, 5,"Shoes");
+
+        int high = 10;
+        int low = 0;
+        assertTrue("Error, intensity is too high", high >= testEndurance.intensity);
+        assertTrue("Error, intensity is too low",  low  <= testEndurance.intensity);
+        System.out.println("Test passed: " + testEndurance.intensity + " is within " + high + " and " + low);
     }
 }
