ce6f66b0d12d11c176f8b65fd0177974b99df57e
olavhl <56083131+olavhl@users.noreply.github.com>
2020-04-02 12:03:20 +0200

    Added setup() in StrengthTest

--- test/ExercisePackage/StrengthTest.java
+++ test/ExercisePackage/StrengthTest.java
@@ -1,15 +1,21 @@
 package ExercisePackage;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StrengthTest {
 
+    Strength testStrength;
+
+    @Before
+    public void setup() {
+        testStrength = new Strength("Olav", 10,60,7,3,17.5,"Manuals");
+    }
+
     // Test made aware of weightsInKg to be an int, when wanted to be a double datatype
     @Test
     public void testToString() {
-
-        Strength testStrength = new Strength("Olav", 10,60,7,3,17.5,"Manuals");
 
         assertEquals("To see if i could use decimal-numbers",17.5, testStrength.weightsInKg, .01);
 
