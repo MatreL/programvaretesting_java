0d6bd6fec605891ea3e093886db2b2def3743745
pkjeserud <pederkjeserud@gmail.com>
2020-04-03 09:44:41 +0200

    MAde some changes to flexibility test

--- test/ExercisePackage/FlexibilityTest.java
+++ test/ExercisePackage/FlexibilityTest.java
@@ -1,16 +1,21 @@
 package ExercisePackage;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class FlexibilityTest {
 
+    Flexibility testFlexibility;
+
+    @Before
+    public void setup() {
+        testFlexibility = new Flexibility("Lil p", 5, 30, 3, 5, "Yoga mat");
+    }
+
     @Test
     public void testToString() {
-
-        Flexibility testFlexibility = new Flexibility("Lil p", 5, 30, 3, 5, "Yoga mat");
-
         assertEquals("Test to check that string equipment works", "Yoga mat", testFlexibility.equipment);
     }
 
