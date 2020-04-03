09ed7625a6e3bd50a59d60caeb60c827b0ffe89a
olavhl <56083131+olavhl@users.noreply.github.com>
2020-04-03 09:47:40 +0200

    Securing valid values in exercise constructor

--- src/ExercisePackage/Exercise.java
+++ src/ExercisePackage/Exercise.java
@@ -9,13 +9,19 @@ public class Exercise {
     }
 
 
-    public Exercise(String name, int intensity, int duration, int repetitions, int sets) {
+    public Exercise(String name, int intensity, int duration, int repetitions, int sets) throws IllegalArgumentException {
         this.name = name;
         this.intensity = intensity;
         this.duration = duration;
         this.repetitions = repetitions;
         this.sets = sets;
+
+        if (name.equals("") || intensity < 0 || intensity >= 10 || duration == 0 || repetitions == 0 || sets == 0){
+            throw new IllegalArgumentException("Invalid value");
+        }
+
     }
+
 
     @Override
     public String toString() {
