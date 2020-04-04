4e6c56c774fecd0934f40d5d5f21d61d222e3e9b
Even Matre Larsen <even@Evens-MacBook-Pro.local>
2020-04-04 10:56:07 +0200

    Added person to Person class

--- src/ExercisePackage/Person.java
+++ src/ExercisePackage/Person.java
@@ -6,5 +6,18 @@ public class Person {
 
     public ArrayList<Exercise> exercises = new ArrayList<>();
     private String name;
+    private String preferredExercise;
+    private int acceptableIntensity;
+
+    public Person(String name, String preferredExercise, int acceptableIntensity){
+        this.name = name;
+        this.preferredExercise = preferredExercise;
+        this.acceptableIntensity = acceptableIntensity;
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("even", "strengt", 2);
+    }
+
 
 }
