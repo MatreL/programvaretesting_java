5468fafa2bab1177905abcddd56d89f7f537de5b
Even Matre Larsen <even@Evens-MacBook-Pro.local>
2020-04-04 08:50:42 +0200

    Added IllegalArgumentException to Endurance class endurance parameter

--- src/ExercisePackage/Endurance.java
+++ src/ExercisePackage/Endurance.java
@@ -8,6 +8,10 @@ public class Endurance extends Exercise {
         super(name, intensity, duration, repetitions, sets);
 
         this.equipment = equipment;
+
+        if (equipment.equals("")) {
+            throw new IllegalArgumentException("Invalid value");
+        }
     }
 
     @Override
