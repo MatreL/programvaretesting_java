af33ba41e534e43ac0325ed9153d21b7298ef9f4
olavhl <56083131+olavhl@users.noreply.github.com>
2020-04-07 12:38:43 +0200

    Created getPreferredExercise() in person.java

--- src/ExercisePackage/Person.java
+++ src/ExercisePackage/Person.java
@@ -14,6 +14,38 @@ public class Person {
         this.acceptableIntensity = acceptableIntensity;
         this.preferredExercise = preferredExercise;
         this.acceptableProgram = acceptableProgram;
+
+        getPreferredExercise();
+    }
+
+    public void getPreferredExercise() {
+
+        String[] acceptedPrefExercise = new String[4];
+        acceptedPrefExercise[0] = "balance";
+        acceptedPrefExercise[1] = "endurance";
+        acceptedPrefExercise[2] = "flexibility";
+        acceptedPrefExercise[3] = "strength";
+
+        String str = preferredExercise.toLowerCase();
+        int i = 0;
+        boolean acceptedPreferredProgram = false;
+
+        while (!acceptedPreferredProgram || i < 4 ) {
+            if (str.equals(acceptedPrefExercise[i])) {
+                acceptedPreferredProgram = true;
+            }
+            i++;
+        }
+
+        if (preferredExercise.equals("")){
+            throw new IllegalArgumentException("Preferred exercise has to be either balance, endurance," +
+                    " flexibility or strength!");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Person p1 = new Person("strength",9,10);
     }
 
 }
