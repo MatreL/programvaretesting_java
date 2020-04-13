7d4025474019a0d8051861ed429101e8bb46a735
alexanderhausken <Hausken123>
2020-04-13 10:22:24 +0200

    Updated BalanceTest

--- test/ExercisePackage/BalanceTest.java
+++ test/ExercisePackage/BalanceTest.java
@@ -10,9 +10,9 @@ public class BalanceTest {
     @Test
     public void testToSting(){
 
-        Balance testBalance = new Balance("Alex","Fitness ball", 2,10,15,4);
+        Balance testBalance = new Balance("Linewalk","Slackline", 2,10,15,4);
 
-        assertEquals("Test to check that string equipment works","Fitness ball",testBalance.equipment );
+        assertEquals("Test to check that string equipment works","Slackline",testBalance.equipment );
     }
 
 }
